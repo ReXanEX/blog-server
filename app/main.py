a23ba9bcac74dc39f40327a1e62df0eba0ab5795
kaylenec <kaylenec@example.com>
2025-10-15 14:59:36 +0300

    app get+post without bd

---
+++ app/main.py
@@ -0,0 +1,28 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+
+app = FastAPI()
+
+id_counter = 1
+posts = []
+
+class Post(BaseModel):
+    title: str
+    content: str
+
+@app.get("/posts")
+def get_posts():
+    return posts
+
+@app.post("/posts")
+def add_post(post: Post):
+    global id_counter
+    row = {
+           "id": id_counter,
+           "title": post.title,
+           "content": post.content
+    }
+    id_counter += 1
+    posts.append(row)
+    return ""
