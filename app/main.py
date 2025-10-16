e8b1bd26b804c258fdbb43fa83a6eff945d8ee9c
kaylenec <kaylenec@example.com>
2025-10-16 03:04:15 +0300

    add PostResponse schema for consistent post API responses

--- app/main.py
+++ app/main.py
@@ -1,23 +1,24 @@
+from typing import List
 from fastapi import FastAPI, Depends
 from sqlalchemy.orm import Session
 
 import models
 from database import get_db, engine
-from schemas import PostCreate
+from schemas import PostCreate, PostResponse
 
 # Create tables at the first start
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-@app.get("/posts")
+@app.get("/posts", response_model=List[PostResponse])
 def get_posts(db: Session = Depends(get_db)):
     return db.query(models.Post).all()
 
-@app.post("/posts")
+@app.post("/posts", response_model=PostResponse, status_code=201)
 def add_post(post: PostCreate, db: Session = Depends(get_db)):
     db_post = models.Post(title=post.title, content=post.content)
     db.add(db_post)
     db.commit()
     db.refresh(db_post)
-    return "db_post"
+    return db_post
--- app/schemas.py
+++ app/schemas.py
@@ -3,3 +3,11 @@ from pydantic import BaseModel
 class PostCreate(BaseModel):
     title: str
     content: str
+
+class PostResponse(BaseModel):
+    id: int
+    title: str
+    content: str
+
+    class Config:
+        from_attributes = True  # allows to work with ORM-models
